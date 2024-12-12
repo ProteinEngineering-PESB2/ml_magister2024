990deecc0ad60137a3a6abfff13695aee089e19e
dMedinaO <david.medina@cebib.cl>
2024-12-12 19:41:24 +0100

    update repo

--- clustering_examples/1_apply_clustering.py
+++ clustering_examples/1_apply_clustering.py
@@ -3,6 +3,7 @@ from sklearn.cluster import (KMeans, DBSCAN, AgglomerativeClustering,
                              SpectralClustering, MeanShift, AffinityPropagation,
                              MiniBatchKMeans, Birch, BisectingKMeans, OPTICS)
 from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
+from sklearn.utils import shuffle
 
 def apply_clustering(dataset, cluster_model, description):
 
@@ -16,6 +17,7 @@ def apply_clustering(dataset, cluster_model, description):
     return cluster_model.labels_, row
 
 df_data = pd.read_csv("estimated_properties.csv")
+df_data = shuffle(df_data, n_samples=3000, random_state=42)
 
 df_values = df_data.drop(columns=["sequence", "Activity"])
 
@@ -27,90 +29,117 @@ df_labels["Activity"] = df_data["Activity"].values
 df_labels["sequence"] = df_data["sequence"].values
 
 # Kmeans
-k=8
-kmeans_instance = KMeans(n_clusters=8)
-description = f"k_means_k_{k}"
-labels, row = apply_clustering(df_values, kmeans_instance, description)
-    
-matrix_exploration.append(row)
+try:
+    print("Try K-Means")
+    k=8
+    kmeans_instance = KMeans(n_clusters=8)
+    description = f"k_means_k_{k}"
+    labels, row = apply_clustering(df_values, kmeans_instance, description)
+        
+    matrix_exploration.append(row)
 
-df_labels[description] = labels
+    df_labels[description] = labels
+except:
+    print("Error K-Means")
 
 # Minibatch
-mini_batch_kmeans = MiniBatchKMeans(n_clusters=k)
-description = f"minibatch_k_means_k_{k}"
-labels, row = apply_clustering(df_values, mini_batch_kmeans, description)
+try:
+    print("Try Minibatch")
+    mini_batch_kmeans = MiniBatchKMeans(n_clusters=k)
+    description = f"minibatch_k_means_k_{k}"
+    labels, row = apply_clustering(df_values, mini_batch_kmeans, description)
 
-matrix_exploration.append(row)
+    matrix_exploration.append(row)
 
-df_labels[description] = labels
+    df_labels[description] = labels
+except:
+    print("Error Minibatch")
 
 # Agglomerative
-k=2
-agglomerative_instance = AgglomerativeClustering(n_clusters=k)
-description = f"Agglomerative_k_{k}"
-labels, row = apply_clustering(df_values, agglomerative_instance, description)
+try:
+    print("Try Agglomerative")
+    k=2
+    agglomerative_instance = AgglomerativeClustering(n_clusters=k)
+    description = f"Agglomerative_k_{k}"
+    labels, row = apply_clustering(df_values, agglomerative_instance, description)
 
-matrix_exploration.append(row)
+    matrix_exploration.append(row)
 
-df_labels[description] = labels
+    df_labels[description] = labels
+except:
+    print("Error Agglomerative")
 
 # Birch
-k=3
-birch_instance = Birch(n_clusters=k)
-description = f"Birch_k_{k}"
-labels, row = apply_clustering(df_values, birch_instance, description)
+try:
+    print("Try Birch")
+    k=3
+    birch_instance = Birch(n_clusters=k)
+    description = f"Birch_k_{k}"
+    labels, row = apply_clustering(df_values, birch_instance, description)
 
-matrix_exploration.append(row)
+    matrix_exploration.append(row)
 
-df_labels[description] = labels
+    df_labels[description] = labels
+except:
+    print("Error Birch")
 
 # Spectral
-k=8
-spectral_instance = SpectralClustering(n_clusters=k)
-description = f"Spectral_k_{k}"
-labels, row = apply_clustering(df_values, spectral_instance, description)
+try:
+    print("Try Spectral")
+    k=8
+    spectral_instance = SpectralClustering(n_clusters=k)
+    description = f"Spectral_k_{k}"
+    labels, row = apply_clustering(df_values, spectral_instance, description)
 
-matrix_exploration.append(row)
+    matrix_exploration.append(row)
 
-df_labels[description] = labels
+    df_labels[description] = labels
+except:
+    print("Error Spectral")
 
-# Bisecting
-bisecting_kmeans = BisectingKMeans(n_clusters=k)
-description = f"Bisecting_k_{k}"
-labels, row = apply_clustering(df_values, bisecting_kmeans, description)
-
-matrix_exploration.append(row)
-
-df_labels[description] = labels
-
-# DBSCAN
-dbscan_instance = DBSCAN()
-description = "DBSCAN"
-labels, row = apply_clustering(df_values, dbscan_instance, description)
-matrix_exploration.append(row)
-df_labels[description] = labels
+try:
+    # DBSCAN
+    print("Try DBSCAN")
+    dbscan_instance = DBSCAN()
+    description = "DBSCAN"
+    labels, row = apply_clustering(df_values, dbscan_instance, description)
+    matrix_exploration.append(row)
+    df_labels[description] = labels
+except:
+    print("Error DBSCAN")
 
 # optics
-optics_instance = OPTICS()
-description = "OPTICS"
-labels, row = apply_clustering(df_values, optics_instance, description)
-matrix_exploration.append(row)
-df_labels[description] = labels
+try:
+    print("Try OPTICS")
+    optics_instance = OPTICS()
+    description = "OPTICS"
+    labels, row = apply_clustering(df_values, optics_instance, description)
+    matrix_exploration.append(row)
+    df_labels[description] = labels
+except:
+    print("Error OPTICS")
 
 # MeanShift
-meanshift_instance = MeanShift()
-description = "MeanShift"
-labels, row = apply_clustering(df_values, meanshift_instance, description)
-matrix_exploration.append(row)
-df_labels[description] = labels
+try:
+    print("Try Meanshift")
+    meanshift_instance = MeanShift()
+    description = "MeanShift"
+    labels, row = apply_clustering(df_values, meanshift_instance, description)
+    matrix_exploration.append(row)
+    df_labels[description] = labels
+except:
+    print("Error Meanshift")
 
 # AffinityPropagation
-AffinityPropagation_instance = AffinityPropagation()
-description = "AffinityPropagation"
-labels, row = apply_clustering(df_values, AffinityPropagation_instance, description)
-matrix_exploration.append(row)
-df_labels[description] = labels
+try:
+    print("Try Affinity")
+    AffinityPropagation_instance = AffinityPropagation()
+    description = "AffinityPropagation"
+    labels, row = apply_clustering(df_values, AffinityPropagation_instance, description)
+    matrix_exploration.append(row)
+    df_labels[description] = labels
+except:
+    print("Error Affinity")
 
 df_performances = pd.DataFrame(data=matrix_exploration, columns=["description", "siluetas",
                                                                  "calinski", "davies"])
